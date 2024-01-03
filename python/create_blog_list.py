0f3529da4b5683bc5e9cc8dd77dff17cdcb092d8
Rebekah Grace <rmgrace@ucdavis.edu>
2024-01-02 22:26:29 -0800

    attempt 3 at linking html correctly

--- python/create_blog_list.py
+++ python/create_blog_list.py
@@ -1,14 +1,15 @@
 from create_header import create_header
 
-def create_blog_list(title, blogs):
+def create_blog_list(title, blogs, is_index = False):
+    header = "html/" if is_index else ""
     string = "<h1>?{title}</h1><ul>".format(title = title)
     for blog in blogs:
         string += """
 <li class = "blogentry">
     <div class = "date"><p>{day}.{month}.{year}<p></div>
-    <div class = "blogtitle"><a href = "/blogs/{route}">{title}</a></div>
+    <div class = "blogtitle"><a href = "{header}/blogs/{route}.html">{title}</a></div>
 </li>
-""".format(day = blog.day, month = blog.month, year = blog.year, route = blog.route, title = blog.title)
+""".format(day = blog.day, month = blog.month, year = blog.year, header = header, route = blog.route, title = blog.title)
 
     string += "</ul>"
 
@@ -28,4 +29,4 @@ def create_list_html(title, blog_list, is_index = False):
 
     bottom = "</body></html>"
 
-    return head + create_header(is_index) + create_blog_list(title, blog_list) + bottom
+    return head + create_header(is_index) + create_blog_list(title, blog_list, is_index) + bottom
