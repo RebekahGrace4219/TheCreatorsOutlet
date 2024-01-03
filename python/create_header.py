6d88d5400cce1a8cfdf50b4e5f064a7f713c65fc
Rebekah Grace <rmgrace@ucdavis.edu>
2024-01-02 20:40:52 -0800

    Current progress, blogs will be produced, but overall site is broken

---
+++ python/create_header.py
@@ -0,0 +1,13 @@
+
+def create_header():
+        return """
+<header>
+<a class="home" href="/">home</a>
+<nav>
+<a href ="/tags">Tags</a>
+<a href ="https://github.com/RebekahGrace4219">GitHub</a>
+<a href = "/about-me">About Me</a>
+<a href = "/contact">Contact</a>
+</nav>
+</header>
+"""
---
+++ python/create_index.py
@@ -0,0 +1,17 @@
+head = """
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/style.css" type="text/css">
+    <title>Blog</title>
+  </head>
+
+
+
+"""
+
+bottom = "</html>"
+
+def write_index_file(blogs):
+    return head + bottom
